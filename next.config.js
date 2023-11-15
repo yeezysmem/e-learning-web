38c85b76cc00c27b18d2388a91ad099f4ad67315
yeezysmem <yeezysmem@gmail.com>
2023-11-15 20:09:28 +0100

    nextConfig: added new domain

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions:true,
+   images: {
+        domains: ['avatars.githubusercontent.com'],
     },
+
 }
 
 module.exports = nextConfig
