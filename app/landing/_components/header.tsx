61692434a17b3e9f0cf00e8290e69531b0015c2a
yeezysmem <yeezysmem@gmail.com>
2024-12-06 17:50:37 +0100

    fix: build bug

--- app/landing/_components/header.tsx
+++ app/landing/_components/header.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/public/logo.svg";
+// import logo from "@/public/logo.svg";
 
 const Header = () => {
   return (
     <header>
         <div>
-            <Image src={logo} width={80} height={80} alt="logo"/>
+            {/* <Image src={logo} width={80} height={80} alt="logo"/> */}
         </div>
       <nav>
         <ul>
